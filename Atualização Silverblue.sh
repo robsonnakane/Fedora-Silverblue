02e6c4cda829bb1552bd2d45830912297c31641d
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2022-06-06 22:16:54 -0300

    Atualização do script

--- Atualização Silverblue.sh
+++ Atualização Silverblue.sh
@@ -18,26 +18,17 @@ rpm-ostree cleanup -m;
 rpm-ostree cleanup -r;
 rpm-ostree cleanup -p;
 rpm-ostree cleanup -b;
-rpm-ostree override reset libdnf;
+rpm-ostree override reset --all;
 ##Conferência dos pacotes##
 rpm-ostree upgrade --check;
 rpm-ostree upgrade --preview;
-rpm-ostree override reset --all;
 ##Atualização do sistema##
 rpm-ostree upgrade;
 
 
-##Instalação do toolbox e programas##
-#rpm-ostree install toolbox --allow-inactive -y;sudo rpm-ostree install neofetch -y;
+##Instalação de programas##
+#rpm-ostree install neofetch; rpm-ostree install idle3; rpm-ostree install simple-scan; rpm-ostree install lshw-gui; rpm-ostree install elementary-code; rpm-ostree install gnome-tweaks; rpm-ostree install java-latest-openjdk;
 
-
-##Dentro do Toolbox##
-#toolbox create
-#toolbox enter
-#sudo dnf install idle3 -y; sudo dnf install lshw-gui -y; sudo dnf install elementary-code -y;sudo dnf install gnome-tweaks -y;sudo dnf install java-latest-openjdk -y;
-
-###Abertura dos programas pelo terminal###
-##elementary-code: $ io.elementary.code
 
 ##Instalação dos programas Flatpak##
 #flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo;flatpak install flathub com.spotify.Client -y;flatpak install flathub org.videolan.VLC -y;flatpak install flathub com.valvesoftware.Steam -y;flatpak install flathub us.zoom.Zoom -y;flatpak install flathub org.kde.kdenlive -y;flatpak install flathub org.onlyoffice.desktopeditors -y;flatpak install flathub com.skype.Client -y;flatpak install flathub com.adobe.Flash-Player-Projector -y;flatpak install flathub org.gnome.Extensions -y;flatpak install flathub org.gnome.Boxes -y;flatpak install flathub org.qbittorrent.qBittorrent -y;flatpak install flathub org.mypaint.MyPaint -y;flatpak install flathub org.mozilla.Thunderbird -y;flatpak install flathub org.chromium.Chromium -y;flatpak install flathub org.raspberrypi.rpi-imager -y;flatpak install flathub org.fedoraproject.MediaWriter -y;flatpak install flathub org.gnome.gedit -y;
